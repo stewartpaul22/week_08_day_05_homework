0e3c3ab92f7d58bd36e58d97f282be2a77c8b3dd
Paul Stewart <stewartpaul22@yahoo.co.uk>
2018-03-17 15:47:24 +0000

    Add properties, constructors, getters and setters to Day class

--- src/main/java/models/Day.java
+++ src/main/java/models/Day.java
@@ -1,4 +1,43 @@
 package models;
 
+import java.util.ArrayList;
+import java.util.GregorianCalendar;
+
 public class Day {
+
+    private int id;
+    private GregorianCalendar dayDate;
+    private ArrayList<Meal> meals;
+
+    public Day() {
+    }
+
+    public Day(GregorianCalendar dayDate) {
+        this.dayDate = dayDate;
+        this.meals = new ArrayList<Meal>();
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public GregorianCalendar getDayDate() {
+        return dayDate;
+    }
+
+    public void setDayDate(GregorianCalendar dayDate) {
+        this.dayDate = dayDate;
+    }
+
+    public ArrayList<Meal> getMeals() {
+        return meals;
+    }
+
+    public void setMeals(ArrayList<Meal> meals) {
+        this.meals = meals;
+    }
 }
