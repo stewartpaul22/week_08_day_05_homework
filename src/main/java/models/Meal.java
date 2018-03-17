c16999e6cace1ead2bb8436483e2721e36aeb879
Paul Stewart <stewartpaul22@yahoo.co.uk>
2018-03-17 14:10:33 +0000

    Add properties, constructors, getters and setters to Meal.java

--- src/main/java/models/Meal.java
+++ src/main/java/models/Meal.java
@@ -1,4 +1,54 @@
 package models;
 
+import enums.MealType;
+
+import java.util.ArrayList;
+
 public class Meal {
+
+    private int id;
+    private String description;
+    private ArrayList<Food> foods;
+    private MealType mealType;
+
+    public Meal() {
+    }
+
+    public Meal(String description, MealType mealType) {
+        this.description = description;
+        this.mealType = mealType;
+        this.foods = new ArrayList<Food>();
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public ArrayList<Food> getFoods() {
+        return foods;
+    }
+
+    public void setFoods(ArrayList<Food> foods) {
+        this.foods = foods;
+    }
+
+    public MealType getMealType() {
+        return mealType;
+    }
+
+    public void setMealType(MealType mealType) {
+        this.mealType = mealType;
+    }
 }
