3265a593450dcc9ab1f23aa3b28fdad704300798
Paul Stewart <stewartpaul22@yahoo.co.uk>
2018-03-17 13:43:43 +0000

    Add constructors, getters and setters to Food and add TestFood.java

--- src/main/java/models/Food.java
+++ src/main/java/models/Food.java
@@ -1,4 +1,41 @@
 package models;
 
 public class Food {
+
+    private int id;
+    private String name;
+    private double calories;
+
+    public Food() {
+    }
+
+    public Food(String name, double calories) {
+        this.name = name;
+        this.calories = calories;
+    }
+
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getCalories() {
+        return calories;
+    }
+
+    public void setCalories(double calories) {
+        this.calories = calories;
+    }
 }
---
+++ src/test/java/TestFood.java
@@ -0,0 +1,25 @@
+import models.Food;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class TestFood {
+
+    private Food food1;
+
+    @Before
+    public void setUp() throws Exception {
+        food1 = new Food("Banana", 100.00);
+    }
+
+    @Test
+    public void hasName() {
+        assertEquals("Banana", food1.getName());
+    }
+
+    @Test
+    public void hasCalories() {
+        assertEquals(100, food1.getCalories(), 0.01);
+    }
+}
